42fdb5d1eff6b29048debf36f446266bb8abc234
iLxlo_ <me@ilxlo.dev>
2023-07-14 15:57:01 +0000

    refactor: Pull the repository instead of checking if an update is available

--- deployment/src/functions/prestart.js
+++ deployment/src/functions/prestart.js
@@ -11,16 +11,30 @@ require("dotenv").config(); // Import the dotenv library and configure it.
  */
 const prestart = async function prestart() {
 
-  if (process.env.AUTO_UPDATE !== "true") return false; // Check if the auto update is not enabled.
-  
-  process.env.GITHUB_VERSION_PREFIX = "v"; // Set the GitHub version prefix to "v".
-  process.env.GITHUB_WORKSPACE = process.cwd(); // Set the GitHub workspace to the current working directory.
+  if (process.env.AUTO_UPDATE !== "true") {
 
-  process.env.GITHUB_REPOSITORY_OWNER = "ilxlo"; // Set the GitHub repository owner to "ilxlo".
-  process.env.GITHUB_REPOSITORY_NAME = "node.js-utils"; // Set the GitHub repository name to "node.js-utils".
+    if (process.env.DEBUG === "true") console.log("The auto update is not enabled for the deployment package"); // Log the info.
+    
+    return false; // Return false.
+  
+  }; // Check if the auto update is not enabled.
+
+  if (process.env.DEBUG === "true") console.log("Starting the process of updating the deployment package"); // Log the info.
+  
+  process.env.GITHUB_WORKSPACE = process.cwd(); // Set the GitHub workspace to the current working directory.
   process.env.GITHUB_REPOSITORY_BRANCH = "main"; // Set the GitHub repository branch to "main".
 
-  this.start(); // Start the process of updating the application.
+  const pulled = await this.pull(); // Start the process of updating the application.
+
+  if (!pulled) {
+
+    if (process.env.DEBUG === "true") console.error("Failed to pull the latest changes of the deployment package with the local repository"); // Log the error.
+
+    return false; // Return false.
+
+  }; // Check if the process of updating the application was not successful.
+
+  if (process.env.DEBUG === "true") console.log("Finished the process of updating the deployment package"); // Log the info.
 
 };
 
